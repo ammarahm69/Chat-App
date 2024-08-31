a8cb527bd96a6cde32510216fafde237183ca211
Ammar Ahmed <ammar69aa59@gmail.com>
2024-08-31 00:10:53 -0700

    spell corrected

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
-import NotFound from './component/NotFound';
+import NotFound from './component/Notfound.jsx';
 import './index.css';
 
 const router = createBrowserRouter([
